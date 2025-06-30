d85a939a8c08cfba6e9ce0236fd34ba0635a944c
Peteroms <omondipeter934@gmail.com>
2025-06-30 11:23:01 +0300

    Ready for deployment to AWS

---
+++ learning_logs/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+class Topic(models.Model):
+    """A topic the user is learning about"""
+    text = models.CharField(max_length=250) # CharField when you want to store a small amount of text, such as name, title etc.
+    date_added = models.DateTimeField(auto_now_add=True) # Record the current date and time automatically.
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.text
+
+class Entry(models.Model):
+    """Something specific learned about a topic"""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE) # Reference to another record in the database
+    text = models.TextField() # This field doesnt need a size limit.
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.text[:50] + "..."  # show just the first 50 characters of text
